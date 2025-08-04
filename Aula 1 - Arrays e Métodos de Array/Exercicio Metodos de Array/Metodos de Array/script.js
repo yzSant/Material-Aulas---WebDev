ab3c1dd8674a012023145a087370175b802d599e
Caio Oliveira <caio.oliveira07@gmail.com>
2025-08-04 00:03:02 -0300

    Conteúdo inicial

---
+++ Aula 1 - Arrays e Métodos de Array/Exercicio Metodos de Array/Metodos de Array/script.js
@@ -0,0 +1,121 @@
+function executarPush() {
+  const array = ['🍎', '🍌'];
+  const resultado = array;
+  document.getElementById('resultado-push').textContent = resultado;
+}
+
+function executarPop() {
+  const array = ['🍎', '🍌', '🍇'];
+  const resultado = array;
+  document.getElementById('resultado-pop').textContent = resultado;
+}
+
+function executarShift() {
+  const array = ['🍎', '🍌', '🍇'];
+  const resultado = array;
+  document.getElementById('resultado-shift').textContent = resultado;
+}
+
+function executarUnshift() {
+  const array = ['🍌', '🍇'];
+  const resultado = 'Resultado da operação unshift';
+  document.getElementById('resultado-unshift').textContent = resultado;
+}
+
+function executarIncludes() {
+  const array =  ['🍎', '🍌', '🍇'];
+  const resultado = array.includes('🥝');
+  document.getElementById('resultado-includes').textContent = resultado;
+}
+
+function executarReverse() {
+  const array = ['🍎', '🍌', '🍇'];
+  const resultado = array;
+  document.getElementById('resultado-reverse').textContent = resultado;
+}
+
+function executarSort() {
+  const array = [3, 1, 4, 2];
+  const resultado = array;
+  document.getElementById('resultado-sort').textContent = resultado;
+}
+
+function executarSlice() {
+  const array = ['🍎', '🍌', '🍇', '🍓'];
+  const resultado = array;
+  document.getElementById('resultado-slice').textContent = resultado;
+}
+
+function executarSplice() {
+  const array = ['🍎', '🍌', '🍇'];
+  const resultado = array;
+  document.getElementById('resultado-splice').textContent = resultado;
+}
+
+
+function executarIndexOf() {
+  const array = ['🍎', '🍌', '🍇'];
+  const resultado = 'Resultado da operação indexOf';
+  document.getElementById('resultado-indexof').textContent = resultado;
+}
+
+function executarJoin() {
+  const array = ['🍎', '🍌', '🍇'];
+  const resultado = 'Resultado da operação join';
+  document.getElementById('resultado-join').textContent = resultado;
+}
+
+function executarForEach() {
+  const array = ['🍎', '🍌', '🍇'];
+  const resultado = 'Resultado da operação forEach';
+  document.getElementById('resultado-foreach').textContent = resultado;
+}
+
+
+function executarMap() {
+  const array = ['🍎', '🍌', '🍇'];
+  const resultado = 'Resultado da operação map';
+  document.getElementById('resultado-map').textContent = resultado;
+}
+
+ function executarMapHTML() {
+    const dispositivos = ['🎮', '🕹️', '💻'];
+  }
+
+function executarFilter() {
+  const array = ['🍎', '🍌', '🍇'];
+  const resultado = 'Resultado da operação filter';
+  document.getElementById('resultado-filter').textContent = resultado;
+}
+
+function executarFind() {
+  const array =  ['🍎', '🍌', '🍇'];
+  const resultado = 'Resultado da operação find';
+  document.getElementById('resultado-find').textContent = resultado;
+}
+
+function executarFindIndex() {
+  const array =  ['🍎', '🍌', '🍇'];
+  const resultado = 'Resultado da operação findIndex';
+  document.getElementById('resultado-findIndex').textContent = resultado;
+}
+
+function executarReduce() {
+  const array = [1, 2, 3, 4];
+  const resultado = 'Resultado da operação reduce';
+  document.getElementById('resultado-reduce').textContent = resultado;
+}
+
+function executarEvery() {
+  const array = [10, 20, 30, 40];
+  const resultado = 'Resultado da operação every';
+  document.getElementById('resultado-every').textContent = resultado;
+}
+
+function executarSome() {
+  const array = ['🍎', '🍌', '🥝'];
+  const resultado = 'Resultado da operação some';
+  document.getElementById('resultado-some').textContent = resultado;
+}
+
+
