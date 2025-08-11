7d4c8c9515068b5a3244862549606bce09ebdba7
Caio Oliveira <caio.oliveira07@gmail.com>
2025-08-11 16:40:43 -0300

    Material Aula CRUD

---
+++ Aula 2 - CRUD/Mini-Rede-Social/script.js
@@ -0,0 +1,20 @@
+// Dados de exemplo dos posts
+let posts = [
+    {
+        text: "Este é o primeiro post",
+        category: "Notícias",
+        image: "https://placedog.net/150?random=1",
+        date: "12/10/2021 12:00:00"
+    },
+    {
+        text: "Este é o segundo post",
+        category: "Dicas",
+        image: "https://placedog.net/150?random=2",
+        date: "12/10/2022 12:00:00"
+    },
+    {
+        text: "Este é o terceiro post teste",
+        category: "Eventos",
+        date: "12/10/2023 12:00:00"
+    }
+];
